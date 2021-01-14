1527c453e2c4cbc3cf9cf6dfa0dc94bd635168ea
Marcrulo <marcus@presutti.eu>
2021-01-14 11:56:52 +0100

    cnn

--- td_gammon/gnubg/bridge.py
+++ td_gammon/gnubg/bridge.py
@@ -81,5 +81,5 @@ def run(host, server_class=HTTPServer, handler_class=Handler, port=8001):
 
 if __name__ == "__main__":
     HOST = 'localhost'  # <-- YOUR HOST HERE
-    PORT = 8003  # <-- YOUR PORT HERE
+    PORT = 8002  # <-- YOUR PORT HERE
     run(host=HOST, port=PORT)
